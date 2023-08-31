feb779c6ca12c25aa2d52cb3a0cf1f333ce88621
Brendan Keyport <brendan.keyport@gmail.com>
2023-08-31 14:50:59 -0700

    0.9.0 - major improvements

--- README.md
+++ README.md
@@ -1,22 +1,47 @@
 # Homebridge Davis Fixed
 This is a Homebridge plugin that allows you to integrate your Davis WeatherLink Live station API.
 
-## Installation
+## Manual Installation
 You must have Homebridge already installed, then just install the plugin by running `npm install -g homebridge-davisfixed`
 
-## How It Works
-This will call your Davis WeatherLink Live station API.  The endpoint it is designed for is /v1/current_conditions.  It will look at the following JSON path for temperature and humidity.
+## GUI installation
+Search for `homebridge-davisfixed` and install. 
 
-* Temperature - `data.conditions[0].temp`
-* Humidity - `data.conditions[0].hum`
+## How It Works
+This will call your Davis WeatherLink Live station API. It will search your Davis Weatherlink local stations for the station of your choice and display data. 
+
+You can also select the internal (indoor) temps as evaluated by the Weatherlink Live control box. 
+
 
 The API will be called once at start up and polled periodically.  The results will be stored in memory, to prevent slowness when opening the Home app.
 
 ## Configuration
 This plugin now does a graphical configuration. Please click "Settings" and follow the prompts. You will find everything needed there. 
 
-I have fixed the following problems: 
-1) There was a conversion problem within the original code. It now converts correctly between units for Homekit. 
-2) Configuration bugs were present, and that is now fixed with the GUI style configuration
+The following is explainations of the fields: 
+
+Device Name -  HomeKit's name for the weather station. 
+
+Manufacturer - This is a string to define who makes the device. Up to you. 
+
+Model - This is to help homekit count the device as unique. Up to you. 
+
+Serial Number - Any combo of letters and numbers - MUST BE UNIQUE. Homekit and some programs needs this to count the device. 
+
+URL - http://ip address/v1/current conditions. Replace "Ip address" with the address of your unit
+
+Trasmitter ID -  the station's ID under "Device Configuration" on weatherlink.com.
+
+Polling interval - How often should it recheck? (Note: Set different values if you're using more than one device on the same Weatherlink hub) 
+
+Units - Does your weather station provide temperature in Fahrenheit or Celsius. Homebridge needs to convert to Celsius for Homekit. 
+
+Use Internal Measurements - This will switch the plugin to use the internal/indoor measurements rather than external. 
+
+Note: The plugin will report "Socket hang up" from time to time. This is normal. I suspect the WLL box isn't able to handle generating the result and scaning the weather stations at the same time. This is why the polling interval is suggested to be different values above. The system will drift naturally and have more successes as it runs. 
+
+Testing is needed - if you use this - please let me know through github issues - if it works, if it don't, if something else is wrong, or if you can get rid of the "socket hang up" issue. 
 
 Original Code by pmoon00, modifications and new work by bkeyport
+
+
--- index.js
+++ index.js
@@ -2,45 +2,39 @@ var Service, Characteristic;
 var request = require("request");
 
 module.exports = function (homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-davisfixed", "davisfixed", davisfixed);
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+	homebridge.registerAccessory("homebridge-davisfixed", "davisfixed", davisfixed);
 }
 
 function davisfixed(log, config) {
-  this.log = log;
+	this.log = log;
 
-  // Config
-  this.url = config["url"];
-  this.name = config["name"];
-  this.manufacturer = config["manufacturer"] || "NotSetByUser";
-  this.model = config["model"] || "NotSetByUser";
-  this.pollingIntervalSeconds = parseInt(config["pollingIntervalSeconds"] || 300);
-  this.temperatureUnitOfMeasure = (config["temperatureUnitOfMeasure"] || "C").toUpperCase();
-  this._timeoutID = -1;
-  this._cachedData = { "temperature": 0, "humidity": 0, "temperaturein": 0, "humidityin": 0 };
-  this.serial = config["serial"] || "NotSetByUser";
-  this.getData(this.url);
+	// Config
+	this.url = config["url"];
+	this.name = config["name"];
+	this.manufacturer = config["manufacturer"] || "NotSetByUser";
+	this.model = config["model"] || "NotSetByUser";
+	this.pollingIntervalSeconds = parseInt(config["pollingIntervalSeconds"] || 300);
+	this.temperatureUnitOfMeasure = (config["temperatureUnitOfMeasure"] || "C").toUpperCase();
+	this._timeoutID = -1;
+	this._cachedData = { "temperature": 0, "humidity": 0, "temperaturein": 0, "humidityin": 0 };
+	this.serial = config["serial"] || "NotSetByUser";
+	this.getData(this.url);
+	this.txid = config["txid"] || 1;
+	this.useInternal = config ["useInternal"] || false; 
 }
 
 davisfixed.prototype = {
-  httpRequest: function (url, body, method, callback) {
-    request({
-      url: url,
-      body: body,
-      method: method
-    },
-    function (error, response, body) { 
-      callback(error, response, body)
-    })
-  },
-
-	getStateHumidityin: function (callback) {
-		callback(null, this._cachedData.humidityin);
-	},
-
-	getStateTemperaturein: function (callback) {
-		callback(null, this._cachedData.temperaturein);
+	httpRequest: function (url, body, method, callback) {
+		request({
+			url: url,
+			body: body,
+			method: method
+		},
+		function (error, response, body) { 
+			callback(error, response, body)
+		})
 	},
 
 	getStateHumidity: function (callback) {
@@ -51,23 +45,23 @@ davisfixed.prototype = {
 		callback(null, this._cachedData.temperature);
 	},
 
-  getServices: function () {
-    var services = [],
-      informationService = new Service.AccessoryInformation();
+	getServices: function () {
+		var services = [],
+			informationService = new Service.AccessoryInformation();
 
-    informationService
-      .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-      .setCharacteristic(Characteristic.Model, this.model)
-      .setCharacteristic(Characteristic.SerialNumber, this.serial);
-    services.push(informationService);
+		informationService
+			.setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+			.setCharacteristic(Characteristic.Model, this.model)
+			.setCharacteristic(Characteristic.SerialNumber, this.serial);
+		services.push(informationService);
 
-    this.temperatureService = new Service.TemperatureSensor(this.name);
+		this.temperatureService = new Service.TemperatureSensor(this.name);
 		this.temperatureService
 			.getCharacteristic(Characteristic.CurrentTemperature)
 			.on("get", this.getStateTemperature.bind(this));
 	services.push(this.temperatureService);
 
-    this.humidityService = new Service.HumiditySensor(this.name);
+		this.humidityService = new Service.HumiditySensor(this.name);
 		this.humidityService
 			.getCharacteristic(Characteristic.CurrentRelativeHumidity)
 			.setProps({minValue: 0, maxValue: 100})
@@ -75,52 +69,72 @@ davisfixed.prototype = {
 	services.push(this.humidityService);
 
 
-    return services;
-  },
+		return services;
+	},
 
-  getData: function (url) {
-    this.httpRequest(url, "", "GET", function (error, response, responseBody) {
-      var queue = function () {
-        if (this._timeoutID > -1) {
-          clearTimeout(this._timeoutID);
-          this._timeoutID = -1;
-        }
+	getData: function (url) {
+		this.httpRequest(url, "", "GET", function (error, response, responseBody) {
+			var queue = function () {
+				if (this._timeoutID > -1) {
+					clearTimeout(this._timeoutID);
+					this._timeoutID = -1;
+				}
 
-        this._timeoutID = setTimeout(function () {
-          this._timeoutID = -1;
-          this.getData(this.url);
-        }.bind(this), this.pollingIntervalSeconds * 1000);
-      }.bind(this);
+				this._timeoutID = setTimeout(function () {
+					this._timeoutID = -1;
+					this.getData(this.url);
+				}.bind(this), this.pollingIntervalSeconds * 1000);
+			}.bind(this);
 
-      if (error) {
-        this.log.error("Request to Davis API failed: %s", error.message);
-        queue();
-        return;
-      }
+			if (error) {
+				this.log.error("Request to Davis API failed: %s", error.message);
+				queue();
+				return;
+			}
 
-      this.log("Request to Davis API succeeded!");
+			this.log("Request to Davis API succeeded!");
 
-      var jsonResponse = JSON.parse(responseBody);
+			var jsonResponse = JSON.parse(responseBody);
 
-      if (jsonResponse.data && (!jsonResponse.data.conditions || jsonResponse.data.conditions.length == 0)) {
-        this.log.error("Response from Davis API doesn't contain expected result.");
-        queue();
-        return;
-      }
+			if (jsonResponse.data && (!jsonResponse.data.conditions || jsonResponse.data.conditions.length == 0)) {
+				this.log.error("Response from Davis API doesn't contain expected result.");
+				queue();
+				return;
+			}
 
-      var weatherResponse = jsonResponse.data.conditions[0];
+			let weather = jsonResponse.data.conditions;
+			let length = weather.length;
 
-      this._cachedData = {
-        "temperature": this.temperatureUnitOfMeasure == "F" ? this.convertFromFahrenheitToCelsius(weatherResponse.temp) : weatherResponse.temp,
-        "humidity": Math.round(weatherResponse.hum),
+			for (let i = 0; i < length; i++) {
+				if (!this.useInternal) {
+					if (weather[i].data_structure_type == 1) {
+						if (weather[i].txid == this.txid) {
+							this.temperature = weather[i].temp;
+							this.humidity = weather[i].hum; 
+						}else{
+								this.temperature = 0;
+								this.humidity = 0; 
+						}
+					}
+				} else {
+				if (weather[i].data_structure_type == 4) {
+					this.temperature = weather[i].temp_in;
+					this.humidity = weather[i].hum_in; 
+				}
+			}
+		}
+
+			this._cachedData = {
+				"temperature": this.temperatureUnitOfMeasure == "F" ? this.convertFromFahrenheitToCelsius(this.temperature) : this.temperature,
+				"humidity": Math.round(this.humidity),
 
 
-      };
-      this.log.debug("Outdoor: Temp %s/%s, Hum %s", this._cachedData.temperature, weatherResponse.temp, this._cachedData.humidity);
-      queue();
-    }.bind(this));
-  },
-  convertFromFahrenheitToCelsius: function (f) { //MUST BE A NUMBER!
-    return parseFloat(((f - 32) * (5 / 9)).toFixed(1));
-  }
+			};
+			this.log.debug("Temp %s, Hum %s", this._cachedData.temperature,  this._cachedData.humidity);
+			queue();
+		}.bind(this));
+	},
+	convertFromFahrenheitToCelsius: function (f) { //MUST BE A NUMBER!
+		return parseFloat(((f - 32) * (5 / 9)).toFixed(1));
+	}
 };
